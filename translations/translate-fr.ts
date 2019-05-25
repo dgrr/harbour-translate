ea45e100c3a3e553a0c0dbf27683573b4a7cb1de
Quentí <33203663+Quenty31@users.noreply.github.com>
2019-05-25 13:25:54 +0200

    Creation of French locale file

---
+++ translations/translate-fr.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>MainPage</name>
+    <message>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Google</source>
+        <translation>Google</translation>
+    </message>
+    <message>
+        <source>Yandex</source>
+        <translation>Yandex</translation>
+    </message>
+    <message>
+        <source>Deepl</source>
+        <translation>Deepl</translation>
+    </message>
+    <message>
+        <source>Translate</source>
+        <translation>Traduire</translation>
+    </message>
+</context>
+<context>
+    <name>PageTranslated</name>
+    <message>
+        <source>Removing</source>
+        <translation>Suppression</translation>
+    </message>
+</context>
+</TS>
